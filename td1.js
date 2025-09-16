8ce9995b36ad2e2d5f23fbcf4f60a171f90bce97
Die Tom <tom.die@etu.unicaen.fr>
2025-09-16 08:52:59 +0200

    ex1

--- td1.js
+++ td1.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+let c = 0;
+let f = c*9/5+32 
+
+console.log("temperature en degrés celsius " + c);
+console.log("temperature en degrés Fahrenheit " + f);
+
+
+let l = 12;
+let L = 50;
+
+let aire = L*L;
+
+console.log("aire du rectangle est"+ aire);
